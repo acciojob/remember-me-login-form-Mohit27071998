1ffaf8f9c49ff11cd3947b85055ebd3e55f87f7c
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-06-21 17:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+let userN = document.getElementById('username').value;
+let pass = document.getElementById('password').value;
+let check = document.getElementById('checkbox').checked;
+
+
+ if (localStorage.getItem("username") && localStorage.getItem("password")) {
+      var existingButton = document.createElement("button");
+      existingButton.id = "existing";
+      existingButton.textContent = "Login as existing user";
+      document.body.appendChild(existingButton);
+    }
+
+
+     function handlesubmit(event){
+	 event.preventDefault();
+		 
+	 if(check){
+		localStorage.setItem('userName','valu')
+		localStorage.setItem('password','valp')
+	}
+			 
+else{
+	localStorage.removeItem('userName');
+	localStorage.removeItem('password');
+}
+  alert("Logged in as " + username);
+		 
+	 }
+document.getElementById("loginForm").addEventListener("submit", handleSubmit);
+ function handleExistingClick() {
+      var savedUsername = localStorage.getItem("username");
+      alert("Logged in as " + savedUsername);
+    }
+
+if (existingButton) {
+      existingButton.addEventListener("click", handleExistingClick);
+ }
+
+
+
+	
